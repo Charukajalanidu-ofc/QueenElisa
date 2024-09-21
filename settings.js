911ea0a78f1f7c673cfc40a435d710aa3398d9f2
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-09-21 19:16:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: 'mongodb://mongo:CfbbnzygILTJqymFDckHyhHhVGOZMRJA@autorack.proxy.rlwy.net:35244',
+  SESSION_ID: '',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
@@ -19,7 +19,7 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
+  MONGODB_URL: 'mongodb://mongo:CfbbnzygILTJqymFDckHyhHhVGOZMRJA@autorack.proxy.rlwy.net:35244', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
